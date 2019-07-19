a843f324896bc90d3b531c2156ac86885bde70ac
Henry Han <henryhan117@hotmail.com>
2019-07-19 10:07:40 -0700

    new commit

---
+++ Desktop/react-ecommerce/src/data.js
@@ -0,0 +1,116 @@
+/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Bose SoundSport In-Ear",
+    img: "img/product-1.png",
+    price: 49,
+    company: "Bose",
+    info:
+     "These Bose® SoundSport® in-ear headphones provide crisp, clear high notes and natural-sounding low tones with TriPort technology. The sweat- and weather-resistant design keeps up with your active lifestyle.",
+	inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Powerbeats3",
+    img: "img/product-2.png",
+    price: 99,
+    company: "Beats Electronics",
+    info:
+     "Take your workout to the next level with Powerbeats³ Wireless earphones, featuring up to 12 hours of battery life to last through multiple workouts and secure-fit earhooks to maximize comfort and stability. With Fast Fuel, a 5-minute charge gives you 1 hour of playback. Sweat and water resistance provides trusted durability and dual-driver acoustics deliver dynamic sound to power your workout.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Apple - EarPods",
+    img: "img/product-3.png",
+    price: 27,
+    company: "Apple",
+    info:
+     "Unlike traditional, circular earbuds, the design of the EarPods is defined by the geometry of the ear. Which makes them more comfortable for more people than any other earbud-style headphones. The speakers inside the EarPods have been engineered to maximize sound output and minimize sound loss, which means you get high-quality audio. The EarPods with Lightning Connector also include a built-in remote that lets you adjust the volume, control the playback of music and video, and answer or end calls with a pinch of the cord.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "SoundSport Wireless",
+    img: "img/product-4.png",
+    price: 199,
+    company: "Bose Electronics",
+    info:
+	"Demanding workouts demand SoundSport® Free wireless headphones. They’re completely free of wires, so you’ll enjoy complete freedom of movement. They’re sweat and weather resistant, too. Bose® StayHear®+ Sport tips are designed to stay secure and comfortable all workout long. And the earbuds are packed full of technology that makes your music sound so clear and powerful, you’ll push past your limits.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Beats by Dr. Dre EP ",
+    img: "img/product-5.png",
+    price: 85,
+    company: "Bose Electronics",
+    info:
+    "Beats EP on-ear headphones delivers masterfully tuned sound. Its battery-free design offers unlimited playback and its sleek, durable frame is reinforced with lightweight stainless steel. Beats EP is an ideal introduction to Beats for any music lover seeking a dynamic listening experience. ",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "JBL - FREE True Wireless",
+    img: "img/product-6.png",
+    price: 149,
+    company: "JBL",
+    info:
+     "Perceive every detail of your music with this pair of JBL wireless headphones. The 10Hz to 22kHz frequency response lets you enjoy the full range of sound, while the in-line microphone provides an effortless switch to hands-free calls. Featuring 5.6mm dynamic drivers, these JBL wireless headphones supply rich, resonant bass for your enjoyment.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Apple AirPods ",
+    img: "img/product-7.png",
+    price: 159,
+    company: "Apple",
+    info:
+     "The new AirPods combine intelligent design with breakthrough technology and crystal-clear sound. Powered by the new Apple H1 headphone chip, AirPods now feature hands-free access to Siri using just your voice. And up to 3 hours of talk time on a single charge.",   inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Skullcandy  INK'D ",
+    img: "img/product-8.png",
+    price: 33,
+    company: "SkullCandy",
+    info:
+      "Pounding bass and brilliant treble combine when you put on a pair of these Skullcandy earphones. Enjoy supreme comfort throughout the day thanks to the comfortable silicone ear gels and the flat cable that minimizes tangles. Simply connect these Skullcandy earphones to your smartphone, tablet or MP3 player using the 3.5mm jack.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel - Black",
+  img: "img/product-1.png",
+  price: 10,
+  company: "google",
+  info:
+    "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
+ 
